4ba072753e41c0dabd3826f37b1db4c9907682b3
ShivaniBhoyar <shivanibhoyar0499@gmail.com>
2025-04-16 14:03:37 +0530

    Add Switch cases

---
+++ E-Com App/src/com/velocity/Test.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DisplayMessages displayMessages = new DisplayMessages();
+		displayMessages.getData();
+
+	}
+
+}
